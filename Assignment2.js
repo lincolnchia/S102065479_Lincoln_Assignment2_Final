951269169c5fb77a283c581898a5132ca76d2388
lincolnchia <73088199+lincolnchia@users.noreply.github.com>
2021-01-25 13:53:48 +0800

    Creating new file

---
+++ Assignment2.js
@@ -0,0 +1,119 @@
+page = 0
+
+animeList(page);
+findAnime(page)
+
+document.getElementById('nextpage').addEventListener('click', function(){
+    $("#mainpage").empty()
+    page+=1
+    animeList(page);
+})
+
+document.getElementById('preivouspage').addEventListener('click', function(){
+    if (page > 0){
+        $("#mainpage").empty()
+        page-=1
+        animeList(page);
+    }
+    else{
+        $("#mainpage").empty()
+        animeList(page);
+    }
+})
+
+
+
+function findAnime(page){
+    document.getElementById('search-btn').addEventListener('click', function(){
+        $("#mainpage").empty()
+        const searchdata = document.getElementById('search-txt').value
+        convertedData = searchdata.replace(/\s/g,"%20")
+        fetch("https://kitsu.io/api/edge/anime?filter[text]="+searchdata)
+        .then(res=>res.json())
+        .then(newdata=>{
+            for (i=0;i<newdata["data"].length;i++){
+                console.log(newdata["data"][i]);
+                console.log("in loop: "+i)
+                createDiv(newdata["data"][i])
+            }
+            function createDiv(anime){
+                // variables needed for div
+                let anime_id = anime["id"]
+                let en_title = anime["attributes"]['canonicalTitle']
+                let jp_title = anime["attributes"]['titles']['ja_jp']
+                let age_rating = anime["attributes"]["ageRating"]
+                let age_Rguide = anime["attributes"]["ageRatingGuide"]
+                let coverImageLink = anime["attributes"]["posterImage"]["original"]
+                let air_date = anime["attributes"]["startDate"]
+                let status = anime["attributes"]["status"]
+    
+                // creating div
+                animeDiv = document.createElement('div');
+                animeDiv.classList.add('anime-card')
+                animeDiv.id = 'animeId';
+                animeDiv.innerHTML = "\
+                <h3>"+anime_id+"</h3>\
+                <h1>"+en_title+"</h1>\
+                <h2>"+jp_title+"</h2>\
+                <img src='"+coverImageLink+"' alt='anime-cover-picture'>\
+                <h3>Air Date: "+air_date+"</h3>\
+                <h3>Status: "+status+"</h3>\
+                <h3>Age Rating: "+age_rating+"</h3>\
+                <h3>Age Guide: "+age_Rguide+"</h3>\
+                ";
+                $(".page").append(animeDiv);
+                document.getElementById("animeId").onclick = function() {directLink()};
+                function directLink(){
+                    console.log("hi")
+                    let slug = anime["attributes"]["slug"]
+                    location.replace("https://kitsu.io/api/edge/anime/"+slug)
+                }
+            }
+        })
+        $(".button-container").empty()
+    })
+}
+
+
+
+
+function animeList(page){
+    fetch("https://kitsu.io/api/edge/anime?page[limit]=5&page[offset]="+5*page)
+    .then(res=>res.json())
+    .then(data=>{
+        for (i=0;i<data["data"].length;i++){
+            console.log(data["data"][i]);
+            console.log("in loop: "+i)
+            createDiv(data["data"][i])
+        }
+        function createDiv(anime){
+            // variables needed for div
+            let anime_id = anime["id"]
+            let en_title = anime["attributes"]['canonicalTitle']
+            let jp_title = anime["attributes"]['titles']['ja_jp']
+            let age_rating = anime["attributes"]["ageRating"]
+            let age_Rguide = anime["attributes"]["ageRatingGuide"]
+            let coverImageLink = anime["attributes"]["posterImage"]["original"]
+            let air_date = anime["attributes"]["startDate"]
+            let status = anime["attributes"]["status"]
+            let slug = anime["attributes"]["slug"]
+            // creating div
+            animeDiv = document.createElement('div');
+            animeDiv.classList.add('anime-card');
+            animeDiv.id = 'animeId';
+            animeDiv.innerHTML = "\
+            <h3>"+anime_id+"</h3>\
+            <h1>"+en_title+"</h1>\
+            <h2>"+jp_title+"</h2>\
+            <img src='"+coverImageLink+"' alt='anime-cover-picture'>\
+            <h3>Air Date: "+air_date+"</h3>\
+            <h3>Status: "+status+"</h3>\
+            <h3>Age Rating: "+age_rating+"</h3>\
+            <h3>Age Guide: "+age_Rguide+"</h3>\
+            <h3>Anime Website ".link("https://kitsu.io/anime/"+slug)+"</h$ git clone --mirror git3>\
+            ";
+            $(".page").append(animeDiv);
+        }
+        
+    })    
+}
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # S102065479_Lincoln_Assignment2_Final
+# Name: Lincoln Chia
+# Class: T03
+# Assignment 2
+
+# 1.Features
+
+## -Shows information about 5 animes at a given page
+## -When clicked on it will direct users to the animes anime page
+## -There is a search bar to allow users to search for their desired anime
+## -buttons at the bottom to allow the useers to scroll through the pages
+
+# 2.Features to be
+
+## - create a favourite page to store users intrested manga
+## - create a page where users can choose what genere they like to search for
+# 3.Technology Used
+## -Jquery
+## -javascript
+## -css
+## -html
+## -usage of api
+## -Adobe XD
+## -British Palate(colour schemes)
+
+# 4.Testing
+## -Postman 
+
+# 5.Git hub link 
+## -https://lincolnchia.github.io/ID_S10206579_LIncolnChIa_Assg02/
+ 
+# 6.Credits
+## -Menu hover underline :https://codepen.io/alphardex/pen/MWWEmLK
+## -Kitsu Api:https://kitsu.docs.apiary.io/
+
+# 7.progress
+## remove the preview file because it is too big.
+## had to start a new repository because couldnt delete preview file
+## This is the Final submittion
